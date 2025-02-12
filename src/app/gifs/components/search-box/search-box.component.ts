e46dec227f5031b130d0b88dfe7e1466e4b2c83f
Juan Cremades <relicary@gmail.com>
2025-02-12 17:22:19 +0100

    Add Gif Search Box component

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  standalone: false,
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css'
+})
+export class SearchBoxComponent {
+
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
