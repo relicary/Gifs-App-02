bd0670cdc6042585c089279e1c3b9a4c07916fda
Juan Cremades <relicary@gmail.com>
2025-02-13 09:53:38 +0100

    Search Component: Recovers a tag list using the GifService

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor( private gifsService:GifsService) { }
+
+  get tags() : string[] {
+    return this.gifsService.tagsHistory;
+  }
 }
