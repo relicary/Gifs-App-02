f89e6926e1b9434bd84b2f2b6e40477d65a028f9
Juan Cremades <relicary@gmail.com>
2025-02-13 12:22:57 +0100

    Gif Service: Load last search if it is in the Local Storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private apiKey: string = 'SmqEZWtgaqApP4UCbg8ywiqBDDYFnALS';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor ( private http: HttpClient ) {}
+  constructor ( private http: HttpClient ) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory(): string[] {
     return [ ...this._tagsHistory ];
@@ -38,6 +40,15 @@ export class GifsService {
     localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
   }
 
+  private loadLocalStorage(): void {
+    if ( !localStorage.getItem('history') ) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if ( this._tagsHistory.length === 0 ) return;
+
+    this.searchTag(this._tagsHistory[0]);
+  }
 
   searchTag( tag: string ): void {
 
