1829d85ddefaba5db6a38bc417f40a7096b3562e
Juan Cremades <relicary@gmail.com>
2025-02-17 10:38:58 +0100

    Added an animation to the loaded image and a delay to see the loader image

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -22,8 +22,9 @@ export class LazyImageComponent implements OnInit{
   }
 
   onLoad() {
-    console.log('Image loaded');
-    this.hasLoaded = true;
+    setTimeout( () => {
+      this.hasLoaded = true;
+    }, 1000);
   }
 
 }
